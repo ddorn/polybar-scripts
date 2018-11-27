1d58af62046c8e87677337aebc8168b28fb6bfa7
ddorn <diego.dorn@free.fr>
2018-11-27 11:55:31 +0100

    Convert cpuload to use the class

--- cpuload.py
+++ cpuload.py
@@ -1,50 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from psutil import cpu_percent
+import psutil
 from time import sleep
 from utils import *
 import click
+from polyclasses import PolybarInfo
 
-LOAD_CRIT = 30
+class CPULoad(PolybarInfo):
 
-def format(load):
-    """
-    Format the load for polybar.
-    """
+    """Display the cpu load for polybar."""
 
-    if load < LOAD_CRIT:
-        return ""
+    LOAD_CRIT = 30
 
-    color = hex_gradient(COLOR_WARM, COLOR_WARN, 1 -  (load - LOAD_CRIT) / (100 - LOAD_CRIT))
+    def print_condition(self, load):
+        return load > self.LOAD_CRIT
 
+    def get_underline_color(self, load):
+        prop = prop_between(load, self.LOAD_CRIT, 100)
+        return hex_gradient(COLOR_WARM, COLOR_WARN, 1 - prop)
 
-    load = str(round(load)) + '% \uf085'
-    load = underline(load, color)
+    def format_short(self, load):
+        """
+        Format the load for polybar.
+        """
 
-    return load
+        return str(round(load)) + '% \uf085'
 
-def test_format(ctx, param, value):
-    if not value:
-        return
+    def get_args_for_test(self):
+        """Generate the args for testing format()."""
 
-    for i in range(0, 101, 5):
-        print(format(i), end='', flush=False)
-    print(flush=1)
-    sleep(10)
+        for i in range(0, 101, 5):
+            yield i,
 
-@click.command()
-@click.option('--test', is_flag=1, callback=test_format, expose_value=False)
-def main():
-    """
-    Give anice cpu load
-    """
-
-    while 1:
-        load = cpu_percent()
-        print(format(load), flush=True)
-        sleep(1)
+    def get_info(self):
+        return psutil.cpu_percent(),
 
 
 if __name__ == "__main__":
-    main()
+    CPULoad().main()
